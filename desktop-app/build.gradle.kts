456b1a5bf082f7685bf2410993c011df7626d9a7
TorentChat Dev <dev@torentchat.local>
2026-07-13 15:05:21 +0700

    fix(desktop): add kotlin plugin.compose (required for Compose Multiplatform + Kotlin 2.0)

--- desktop-app/build.gradle.kts
+++ desktop-app/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm") version "2.0.0"
     id("org.jetbrains.compose") version "1.6.11"
+    kotlin("plugin.compose") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
 }
 
